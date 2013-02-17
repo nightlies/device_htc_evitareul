0200d7f051da7678e5504128769af63b636cf862
Rob Shannon <spirit2457@gmail.com>
2013-02-17 21:55:42 +0000

    On My way to having some sleep

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ ENABLE_WEBGL := true
 # EGL settings
 USE_OPENGL_RENDERER := true
 BOARD_EGL_NEEDS_LEGACY_FB := true
-BOARD_EGL_CFG := device/htc/enrc2b/configs/egl.cfg
+BOARD_EGL_CFG := device/htc/evitareul/configs/egl.cfg
 
 # Graphics - Skia
 BOARD_USE_SKIA_LCDTEXT := true
@@ -88,11 +88,7 @@ WIFI_DRIVER_FW_PATH_P2P     := "/system/etc/firmware/fw_bcm4334_p2p.bin"
 # BT
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/htc/enrc2b/bluetooth
-
-# HTC ril compatability
-BOARD_USE_NEW_LIBRIL_HTC := true
-TARGET_PROVIDES_LIBRIL := device/htc/enrc2b/proprietary/lib/libhtc-ril.so
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/htc/evitareul/bluetooth
 
 # USB
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun0/file"
@@ -118,6 +114,7 @@ TARGET_PROVIDES_LIBRIL := device/htc/evitareul/proprietary/lib/libril.so
 
 # Enable WEBGL in WebKit
 ENABLE_WEBGL := true
+
 # Partition Info
 #dev:        size     erasesize name
 #mmcblk0p5: 00800000 00001000 "recovery"
