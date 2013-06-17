13dde0d9681427daa03c728a4804cb11a3629f28
Rob <spirit2457@gmail.com>
2013-06-17 16:19:00 +0100

    renamed files
    Change-Id: I8970fb4d00b16a958ec837ec098fb7966f7d6059

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/evitareul/device_evitareul.mk)
+$(call inherit-product, device/htc/evitareul/cm_evitareul.mk)
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=evitareul BUILD_FINGERPRINT=cingular_us/evitareul/evitareul:4.1.2/JRO03C/122548.9:user/release-keys PRIVATE_BUILD_DESC="1.15.502.9 CL122548 release-keys" BUILD_NUMBER=122548
